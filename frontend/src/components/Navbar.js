c253e46f481b2060fd77a83667501bc3dea55459
Hayesdb1742 <hayesdbentley1@live.com>
2023-04-30 12:33:51 -0400

    added react router

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css'
-<<<<<<< HEAD
 import gHubLogo from "../images/github-mark-white.png"
 import Resume from "./Resume"
 
-=======
-import gLogo from '../images/github-mark-white.png'
->>>>>>> 18a3368d63ad496b20d9a17697aeb3eb13f0076d
 function Navbar() {
   return (
     <nav className="navbar">
@@ -30,11 +26,7 @@ function Navbar() {
           </li>
           <li>
             <a href="https://github.com/Hayesdb1742?tab=repositories" target="_blank" rel="noopener noreferrer">
-<<<<<<< HEAD
                 <img src={gHubLogo} alt="Github" width="32" height="32" />
-=======
-                <img src={gLogo} width="32" height="32" />
->>>>>>> 18a3368d63ad496b20d9a17697aeb3eb13f0076d
             </a>
           </li>
           <li>
